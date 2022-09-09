03b2bf97025afa147d37a652261fd1f8f0562017
Bruno Lemos <brunomlemos.ti@gmail.com>
2022-09-08 22:10:24 -0300

    praticando com exercícios

---
+++ exercicio-3.js
@@ -0,0 +1,14 @@
+// # Exercício 03
+
+// ## Desconto do tênis
+
+let valorTotal, desconto, porcentagem, meuDinheiro;
+
+valorTotal = 110
+meuDinheiro = 50
+
+desconto = valorTotal - meuDinheiro; //Valor do desconto em reais
+
+porcentagem = (desconto / valorTotal) * 100; //Valor do desconto em porcentagem
+
+console.log(porcentagem.toFixed(2)); //Utilizado do toFixed para mostrar apenas 2 casa decimais

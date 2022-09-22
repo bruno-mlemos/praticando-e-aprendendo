d31c7c09456dff9d026409ac42709199a7980f06
Bruno Lemos <brunomlemos.ti@gmail.com>
2022-09-22 01:42:29 -0300

    exercicio de prática

---
+++ exercicio-21-8.js
@@ -0,0 +1,15 @@
+const idade = 23;
+const possuiPatologia = false;
+const altura = 180;
+const ehEstudante = true;
+
+if (idade < 12 || idade > 65 || possuiPatologia || altura < 150) {
+    console.log("ACESSO NEGADO");
+
+} else if (ehEstudante || idade < 18) {
+    console.log("10 reais");
+
+} else {
+    console.log("20 reais");
+}
+

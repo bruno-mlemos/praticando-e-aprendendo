e3866c0adfc3b5912b69e67a34d94a7b080c5ffc
Bruno Lemos <brunomlemos.ti@gmail.com>
2022-10-19 21:38:59 -0300

    exercicio finalizado[A

---
+++ exercicio-43-2.js
@@ -0,0 +1,27 @@
+const carros = [{
+    marca: "Renault",
+    modelo: "Logan",
+    ano: 2021,
+    cor: "vermelho",
+    quantidade_portas: 4,
+    automatico: false
+},
+{
+    marca: "Renault",
+    modelo: "Clio",
+    ano: 2012,
+    cor: "preto",
+    quantidade_portas: 2,
+    automatico: false
+},
+{
+    marca: "Renault",
+    modelo: "Clio",
+    ano: 2012,
+    cor: "preto",
+    quantidade_portas: 2,
+    automatico: false
+}]
+
+console.log(carros);
+

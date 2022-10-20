22784bcd59791e030edb4232fd08bb77f972fffe
Bruno Lemos <brunomlemos.ti@gmail.com>
2022-10-19 21:37:51 -0300

    exercicio finalizado

---
+++ exercicio-42-2.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua A. A. Soares",
+    numero: 64,
+    complemento: "",
+    bairro: "Graças",
+    cep: 54543123,
+    cidade: "Recife",
+    estado: "Pernambuco",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Bruno",
+    email: "brunomlb@outlook.com",
+    telefone: "8199299999",
+    data_nascimento: "01/01/90",
+    endereco
+}
+
+console.log(usuario)

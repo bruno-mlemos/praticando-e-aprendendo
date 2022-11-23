a5118ea93c8a7636bad2c33e87fa9b2596f4b143
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-22 23:23:27 -0300

    Add files via upload

---
+++ exercicios-backend-variaveis/05/index.js
@@ -0,0 +1,15 @@
+// # Exercício 05
+
+// ## Calcular a distância entre dois pontos
+
+let x1, x2, y1, y2, distancia;
+
+x1 = 1
+y1 = 1
+
+x2 = 1
+y2 = 4
+
+distancia = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+console.log(`A distancia entre x e y é ${distancia.toFixed(2)}`);

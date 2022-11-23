4c4739082962899ac0a017de37f8cd1554a8cee8
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-23 17:45:37 -0300

    Add files via upload

---
+++ exercicios-backend-condicionais/04/index.js
@@ -0,0 +1,44 @@
+const ladoA = 4;
+const ladoB = 4;
+
+//seu código aqui
+
+if (ladoA === ladoB) {
+
+    if (ladoA === 0) {
+        console.log("Carroça de Branco");
+    }
+
+    if (ladoA === 1) {
+        console.log("Carroça de Ás");
+
+    }
+
+    if (ladoA === 2) {
+
+        console.log("Carroça de Duque");
+    }
+
+    if (ladoA === 3) {
+
+        console.log("Carroça de Terno");
+    }
+
+    if (ladoA === 4) {
+
+        console.log("Carroça de Quadra");
+    }
+
+    if (ladoA === 5) {
+
+        console.log("Carroça de Quina");
+    }
+
+    if (ladoA === 6) {
+
+        console.log("Carroça de Sena");
+    }
+
+} else {
+    console.log('NÃO É UMA CARROÇA');
+}

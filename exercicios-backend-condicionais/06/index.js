874b1e3a1e255f1c6b84c00ffc1b5acec6ff9d24
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-23 17:45:53 -0300

    Add files via upload

---
+++ exercicios-backend-condicionais/06/index.js
@@ -0,0 +1,13 @@
+const alturaEmCm = 196;
+
+if (alturaEmCm < 180) {
+    console.log("Reprovado");
+} else if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+    console.log("LÍBERO");
+} else if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+    console.log("PONTEIRO");
+} else if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+    console.log("OPOSTO");
+} else {
+    console.log("CENTRAL");
+}

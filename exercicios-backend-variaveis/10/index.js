8689272a4f274d5f97954392a9eaa8ac6a7bf19f
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-22 23:25:22 -0300

    Add files via upload

---
+++ exercicios-backend-variaveis/10/index.js
@@ -0,0 +1,11 @@
+// # Exercício 10
+
+// ## Delta
+
+let a, b, c, delta;
+
+a = 5; b = 5; c = 1;
+
+delta = (b ** 2) - 4 * a * c;
+
+console.log(`O valor de Delta com os valores de \n\na = ${a} \nb = ${b} \nc = ${c}\n\né ${delta}`);

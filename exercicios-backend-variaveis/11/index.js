131022be8159a3411976910510702395c6e6bcdb
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-22 23:25:38 -0300

    Add files via upload

---
+++ exercicios-backend-variaveis/11/index.js
@@ -0,0 +1,13 @@
+// # Exercício 11
+
+// ## Volume de uma esfera
+
+let volumeParcial, diametro;
+
+diametro = 6;
+
+volumeParcial = (4 / 3) * ((diametro / 2) ** 3);
+
+console.log(`O volume de uma esfera de raio ${diametro / 2} é ${volumeParcial} PI.`);
+
+

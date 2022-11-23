ab9b33cffdef947cacfa944cc5763d24c8080b19
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-22 23:24:07 -0300

    Add files via upload

---
+++ exercicios-backend-variaveis/06/index.js
@@ -0,0 +1,12 @@
+// # Exercício 06
+
+// ## Velocidade média
+
+let velocidade, distPercorrida, tempo;
+
+distPercorrida = 500; //em metros
+tempo = 10 // em segundos
+
+velocidade = (distPercorrida / tempo) * 3.6; //em km/h
+
+console.log(velocidade);

b31ab2b65f83d82903fe017db46d48948a4233f0
Bruno Lemos <111924568+bruno-mlemos@users.noreply.github.com>
2022-11-25 23:51:10 -0300

    Add files via upload

---
+++ exercicios-backend-condicionais/10/index.js
@@ -0,0 +1,16 @@
+const caractere = 9;
+
+if (caractere === "A" || caractere === "E" || caractere === "I" || caractere === "O" || caractere === "U" || caractere === "a" || caractere === "e" || caractere === "i" || caractere === "o" || caractere === "u") {
+
+    if (caractere == "a" || caractere === "e" || caractere === "i" || caractere === "o" || caractere === "u") {
+        console.log("Vogal minuscula");
+    } else {
+        console.log("Vogal maiuscula");
+    }
+
+} else if (caractere == "1" || caractere == "2" || caractere == "9" || caractere == "3" || caractere == "4" || caractere == "5" || caractere == "6" || caractere == "0" || caractere == "7" || caractere == "8") {
+    console.log("Numero");
+} else {
+    console.log("Consoante")
+}
+
